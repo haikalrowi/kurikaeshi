d0583b26e49bc5bf4a9c77080857cdc5e6511f36
Haikal Rowi <93899076+haikalrowi@users.noreply.github.com>
2024-06-13 11:01:12 +0700

    766883

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import "@/styles/globals.css";
+import { NextUIProvider } from "@nextui-org/react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "kurikaeshi",
+  description: "A platform for repetitive chat",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <NextUIProvider>{children}</NextUIProvider>
+      </body>
+    </html>
+  );
+}
