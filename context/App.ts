ab502d11420f5779397462df7ba9528475d4e58c
Haikal Rowi <93899076+haikalrowi@users.noreply.github.com>
2024-06-13 20:19:33 +0700

    Update App.ts

--- context/App.ts
+++ context/App.ts
@@ -1,5 +1,5 @@
 import { appContext } from "@/lib/action";
 import { createContext } from "react";
 
-export type AppContextType = Partial<Awaited<ReturnType<typeof appContext>>>;
-export const AppContext = createContext<AppContextType>({});
+export type AppContextType = Awaited<ReturnType<typeof appContext>>;
+export const AppContext = createContext<AppContextType>({} as AppContextType);
